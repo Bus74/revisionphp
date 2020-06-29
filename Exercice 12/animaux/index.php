a66dd0b2bec39ae91cf3b0a4c3a76b3f56c73e3f
Yan <athenyan74@hotmail.com>
2020-06-29 10:49:20 +0200

    exo12 suite et fin

--- Exercice 12/animaux/index.php
+++ Exercice 12/animaux/index.php
@@ -60,6 +60,32 @@ if(
 }
 
 ?>
+<!--affichage des animaux-->
+
+<?php
+
+ // Connexion à la BDD
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=animals;charset=utf8', 'root', '');
+
+    // Ligne permettant d'afficher les erreurs SQL à l'écran
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(Exception $e){
+
+    die('Il y a un problème avec la bdd : ' . $e->getMessage());
+}
+$response = $bdd->query('SELECT * FROM animals ');
+
+
+
+// Récupération de tous les résultats de la requête sous forme d'arrays associatifs
+$animals = $response->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -68,6 +94,45 @@ if(
     <title>15 - Formulaire et Insertion dans une base de données</title>
 </head>
 <body>
+<style>
+
+body{
+    font-family: sans-serif;
+    margin:0;padding:0;
+}
+
+p.error{
+    color: red;
+    text-align: center;
+    font-size: 1.4rem;
+}
+
+h1.main-title{
+        text-align: center;
+}
+
+div.container{
+margin: 40px;
+}div.container table{
+margin: auto;
+border: medium solid #6495ed;
+border-collapse: collapse;
+text-align: center;
+}
+
+div.container table tr th {
+border: thin solid #6495EE;
+padding: 10px;
+min-width: 150px;
+background-color: #D0E3FA;
+}
+
+div.container table tr td {
+border: 1px solid #6495EE;
+padding: 8px;
+}
+
+</style>
 
     <h1>15 - Formulaire et Insertion dans une base de données</h1>
     
@@ -83,7 +148,7 @@ if(
     // Si message de succès existe, on l'affiche. Sinon on affiche le formulaire
     if(isset($successMessage)){
         echo '<p style="color:green;">' . $successMessage . '</p>';
-    } else {
+    } 
     
         ?>
 
@@ -94,11 +159,56 @@ if(
             <input type="submit">
         </form>
 
+        
+
+
+    <h1 class="main-title">Liste d'animaux</h1>
+
+    <div class="container">
+
         <?php
-    }
-    ?>
+        // Si il y a des animaux
+        if(!empty($animals)){
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Espèce</th>
+                        <th>Date de naissance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                   
+                    // Affichage des animaux
+                    foreach($animals as $animal){
+                        $dateInObject= new DateTime($animal['birthdate']);
+                        echo '
+                            <tr>
+                                <td>' . ucfirst( htmlspecialchars( $animal['name'] ) ) . '</td>
+                                <td>' . ucfirst( htmlspecialchars( $animal['species'] ) ) . '</td>
+                                <td>' . $dateInObject->format('d/m/Y') . '</td>
+                            </tr>
+                        ';
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+        } else {
+            echo '<p class="error">Aucun animal à afficher</p>';
+        }
+        ?>
+    </div>
 
 </body>
 </html>
+
+
+ 
+
+
+    
 
 
